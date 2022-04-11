28c7ef43b7f0ad0c15026ee758635c1b91e71a21
Georgy <georgy.matskevich@gmail.com>
2022-04-11 18:50:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main (String[] args){
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        Stream<Person> stream1 = persons.stream();
+        long ages = stream1.filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println(ages);
+
+        Stream<Person> stream2 = persons.stream();
+        List<String> a = stream2.filter(person -> person.getAge() >= 18 )
+                .filter(person -> person.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        System.out.println(a);
+
+        Stream<Person> stream3 = persons.stream();
+        List<Person> people = stream3.filter(person -> person.getEducation().equals(Education.HIGHER))
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+        System.out.println(people);
+    }
+}
